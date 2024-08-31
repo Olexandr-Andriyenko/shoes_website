4e8ad5847a664f0863ef3dd28629011029a0fe1d
Olex <Olexandr@co-dex.de>
2024-08-31 15:26:37 +0200

    feat: add data and extend Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,33 @@
-function Nav() {
-  return <div>Nav</div>;
-}
+import { headerLogo } from '../assets/images';
+import { hamburger } from '../assets/icons';
+import { navLinks } from '../constants';
+
+const Nav = () => {
+  return (
+    <header className='padding-x py-8 z-10 w-full'>
+      <nav className='flex justify-between items-center max-container'>
+        <a href='/'>
+          <img
+            src={headerLogo}
+            alt='Logo'
+            width={130}
+            height={29}
+          ></img>
+        </a>
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map(item => (
+            <a
+              key={item.href}
+              href={item.href}
+              className='font-montserrat leading-normal text-lg text-slate-gray'
+            >
+              {item.label}
+            </a>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Nav;
