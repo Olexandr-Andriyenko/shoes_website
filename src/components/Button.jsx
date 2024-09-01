903b3b15e85b09883c73d50a3cdf326c27175f22
Olex <Olexandr@co-dex.de>
2024-09-01 12:14:19 +0200

    feat: finish SpecialOffer section and button adjustments

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,21 @@
 import PropTypes from 'prop-types';
 
-function Button({ label, iconURL }) {
+function Button({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+}) {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : 'bg-coral-red text-white border-coral-red'
+      } rounded-full`}
+    >
       {label}{' '}
       {iconURL && (
         <img
@@ -17,8 +30,11 @@ function Button({ label, iconURL }) {
 
 // PropTypes-Definitionen hinzufügen
 Button.propTypes = {
-  label: PropTypes.string.isRequired, // label ist eine erforderliche Zeichenkette
-  iconURL: PropTypes.string.isRequired, // iconURL ist eine erforderliche Zeichenkette
+  label: PropTypes.string.isRequired,
+  iconURL: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string.isRequired,
+  textColor: PropTypes.string.isRequired,
+  borderColor: PropTypes.string.isRequired,
 };
 
 export default Button;
